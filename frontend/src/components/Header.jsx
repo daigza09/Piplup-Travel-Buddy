67c2602ea0ffb98650ea445c3ef1766bb769697b
zuniiix <lisamzuni143@gmail.com>
2024-02-10 16:26:39 -0600

    Nav bar in progress

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
-function Header(){
-    return(
+
+function Header() {
+    return (
         <header className="flex items-center justify-between p-4 border-b-2 border-[#644536] md:px-6">
             <div className="flex items-center md:space-x-10">
-                Testing
+                <Link to="/user">User Information</Link>
+                <Link to="/flight-information">Flight Information</Link>
             </div>
         </header>
     );
 }
+
 export default Header;
+
