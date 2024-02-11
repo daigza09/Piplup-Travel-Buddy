1b43d714b5072f1ce7f824317c567099835447ae
Colin <cabb007@live.com>
2024-02-10 18:45:01 -0600

    resolved merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 import './App.css';
 
 function App() {
@@ -10,10 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="/home" />} />
         <Route path="home" element={<Home />}/>
-<<<<<<< HEAD
-=======
         <Route path="search" element={<Search />}/>
->>>>>>> e07bafb87fda5f738757e692bcf3246142a53ab0
       </Route>
     </Routes>
     </>
