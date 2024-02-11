fca4e38c4a806e4682fe2402b62d14975767cf5f
Jynxae <realkhalily@hotmail.com>
2024-02-11 03:10:32 -0600

    Updated the Search.jxs with a new search engine

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -2,55 +2,6 @@ import React from "react";
 import destinationBG from "../assets/destinationBG.jpg";
 import "./Search.css";
 function Search() {
-  const originInput = document.getElementById("origin-input");
-  const originOptions = document.getElementById("origin-options");
-  const destinationInput = document.getElementById("destination-input");
-  const destinationOptions = document.getElementById("destination-options");
-  const flightTypeSelect = document.getElementById("flight-type-select");
-  const departureDateInput = document.getElementById("departure-date-input");
-  const returnDate = document.getElementById("return-date");
-  const returnDateInput = document.getElementById("return-date-input");
-  const travelClassSelect = document.getElementById("travel-class-select");
-  const adultsInput = document.getElementById("adults-input");
-  const childrenInput = document.getElementById("children-input");
-  const infantsInput = document.getElementById("infants-input");
-  const searchButton = document.getElementById("search-button");
-
-  const reset = () => {
-    originInput.value = "";
-    destinationInput.value = "";
-    flightTypeSelect.value = "one-way";
-    departureDateInput.valueAsDate = new Date();
-    returnDateInput.valueAsDate = new Date();
-    returnDate.classList.add("d-none");
-    travelClassSelect.value = "ECONOMY";
-    adultsInput.value = 1;
-    childrenInput.value = 0;
-    infantsInput.value = 0;
-    searchButton.disabled = true;
-  };
-
-  document.body.addEventListener("input", () => {
-    searchButton.disabled = !originInput.value || !destinationInput.value;
-  });
-  originInput.addEventListener("input", () => {
-    // autocomplete
-  });
-  destinationInput.addEventListener("input", () => {
-    // autocomplete
-  });
-  flightTypeSelect.addEventListener("change", () => {
-    if (flightTypeSelect.value === "one-way") {
-      returnDate.classList.add("d-none");
-    } else {
-      returnDate.classList.remove("d-none");
-    }
-  });
-  searchButton.addEventListener("click", async () => {
-    // search
-  });
-  reset();
-
   return (
     <main
       className="relative h-screen bg-cover"
@@ -269,6 +220,55 @@ function Search() {
       </div>
     </main>
   );
+
+  const originInput = document.getElementById("origin-input");
+  const originOptions = document.getElementById("origin-options");
+  const destinationInput = document.getElementById("destination-input");
+  const destinationOptions = document.getElementById("destination-options");
+  const flightTypeSelect = document.getElementById("flight-type-select");
+  const departureDateInput = document.getElementById("departure-date-input");
+  const returnDate = document.getElementById("return-date");
+  const returnDateInput = document.getElementById("return-date-input");
+  const travelClassSelect = document.getElementById("travel-class-select");
+  const adultsInput = document.getElementById("adults-input");
+  const childrenInput = document.getElementById("children-input");
+  const infantsInput = document.getElementById("infants-input");
+  const searchButton = document.getElementById("search-button");
+
+  const reset = () => {
+    originInput.value = "";
+    destinationInput.value = "";
+    flightTypeSelect.value = "one-way";
+    departureDateInput.valueAsDate = new Date();
+    returnDateInput.valueAsDate = new Date();
+    returnDate.classList.add("d-none");
+    travelClassSelect.value = "ECONOMY";
+    adultsInput.value = 1;
+    childrenInput.value = 0;
+    infantsInput.value = 0;
+    searchButton.disabled = true;
+  };
+
+  document.body.addEventListener("input", () => {
+    searchButton.disabled = !originInput.value || !destinationInput.value;
+  });
+  originInput.addEventListener("input", () => {
+    // autocomplete
+  });
+  destinationInput.addEventListener("input", () => {
+    // autocomplete
+  });
+  flightTypeSelect.addEventListener("change", () => {
+    if (flightTypeSelect.value === "one-way") {
+      returnDate.classList.add("d-none");
+    } else {
+      returnDate.classList.remove("d-none");
+    }
+  });
+  searchButton.addEventListener("click", async () => {
+    // search
+  });
+  reset();
 }
 
 export default Search;
