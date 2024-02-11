0363add1573d101ef192ebf9c23dc1a3fb14d957
Jynxae <realkhalily@hotmail.com>
2024-02-10 22:41:20 -0600

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,25 @@ import About from "./pages/About";
 import Account from "./pages/Account";
 import CreateAccount from "./pages/CreateAccount";
 import LiveChat from "./pages/LiveChat";
-import './App.css';
+import UserInformation from "./pages/UserInformation";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/home" />} />
-        <Route path="home" element={<Home />}/>
-        <Route path="search" element={<Search />}/>
-        <Route path="Account" element={<Account />}/>
-        <Route path="CreateAccount" element={<CreateAccount />} />
-        <Route path="about" element={<About />}/>
-        <Route path="LiveChat" element = {<LiveChat/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="Account" element={<Account />} />
+          <Route path="CreateAccount" element={<CreateAccount />} />
+          <Route path="about" element={<About />} />
+          <Route path="LiveChat" element={<LiveChat />} />
+          <Route path="UserInformation" element={<UserInformation />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
-export default App
+export default App;
