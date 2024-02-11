beea41a9eb67647e61efc44a44e9d48569f69597
Jynxae <realkhalily@hotmail.com>
2024-02-10 23:03:05 -0600

    updating userinformation

--- frontend/src/pages/UserInformation.jsx
+++ frontend/src/pages/UserInformation.jsx
@@ -3,10 +3,14 @@ import homeBG from "../assets/homeBG.jpg";
 import destinationBG from "../assets/destinationBG.jpg";
 
 function UserInfo() {
-  <main
-    className="relative h-screen bg-cover flex items-center"
-    style={{ backgroundImage: `url(${destinationBG})` }}
-  ></main>;
+  return (
+    <main
+      className="relative h-screen bg-cover flex items-center"
+      style={{ backgroundImage: `url(${destinationBG})` }}
+    >
+      <div className="bg-white rounded-lg p-8"></div>
+    </main>
+  );
 }
 
 export default UserInfo;
