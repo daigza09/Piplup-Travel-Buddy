42f05615c2542d09df012a572554a730271490e8
daigza09 <daigza09@gmail.com>
2024-02-11 01:46:31 -0600

    testing

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/users', userRoutes); // Mount user routes
 
 // Start the server
-const PORT = process.env.PORT || 8080; // Use the provided port or default to 5000
+const PORT = process.env.PORT || 8080; // Use the provided port or default to 8080
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
