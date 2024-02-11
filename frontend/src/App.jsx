d2738a0cdb4a4f5d1c876e50495a3e1d218ecf69
Colin <cabb007@live.com>
2024-02-10 19:30:16 -0600

    added route for About

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import About from "./pages/About";
+
 import './App.css';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <Route index element={<Navigate to="/home" />} />
         <Route path="home" element={<Home />}/>
         <Route path="search" element={<Search />}/>
+        <Route path="about" element={<About />}/>
       </Route>
     </Routes>
     </>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,42 +1,12 @@
-import React from "react";
-import destinationBG from "../assets/destinationBG.jpg";
-import "./Search.css";
+import { Link } from "react-router-dom";
 
-function Search() {
-  const dataDestination = ["Houston", "Austin", "Dallas"];
-  const dataArrival = ["Washington", "california", "kentucky"];
-
+function About(){
   return (
-    <main
-      className="relative h-screen bg-cover"
-      style={{ backgroundImage: `url(${destinationBG})` }}
-    >
-      <div className="absolute inset-0 backdrop-blur-sm"></div>
-      <div className="container mx-auto text-center relative flex items-center justify-center h-full">
-        <div className="bg-white py-14 px-40 bg-opacity-70">
-          <h1 className="text-7xl font-bold text-[#05204A] mb-4">
-            Looking for a flight?
-          </h1>
-          <p className="text-xl text-[#05204A] mb-8">Let us help!</p>
-
-          <div className="main">
-            <input list="destinationList" placeholder="Destination" />
-            <datalist id="destinationList">
-              {dataDestination.map((op) => (
-                <option key={op}>{op}</option>
-              ))}
-            </datalist>
-
-            <input list="arrivalList" placeholder="Arrival" />
-            <datalist id="arrivalList">
-              {dataArrival.map((op) => (
-                <option key={op}>{op}</option>
-              ))}
-            </datalist>
-          </div>
-        </div>
+    <main>
+      <div className="flex items-center justify-between bg-[#ffffff] p-4">
+        <h1 className="text-5xl">Who we are</h1>
       </div>
     </main>
   );
 }
-export default Search;
+export default About;
